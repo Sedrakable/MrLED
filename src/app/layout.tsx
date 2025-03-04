29130be37a28ec3e7aa151c57ada31d0dd4bca6b
Sedrakable <sedrak005@gmail.com>
2025-03-03 20:24:57 -0500

    added analitics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 export default async function RootLayout({
   children,
@@ -9,6 +10,7 @@ export default async function RootLayout({
     <>
       {children}
       <Analytics />
+      <GoogleAnalytics gaId="G-9L7V0EC0B8" />
     </>
   );
 }
